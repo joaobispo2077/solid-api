2ccfcc73cbfca4450c602e48815c182a7bf4d88b
João Bispo <joaobispo2077@gmail.com>
2021-06-17 11:59:20 -0300

    feat: added create user controlller and connect layers

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import { createUserController } from './useCases/CreateUser';
 
 const router = Router();
 
 router.post('/', (req, res, next) => {
-  return res.status(201).send();
+  return createUserController.handle(req, res);
 });
 
 export { router };
---
+++ src/useCases/CreateUser/CreateUserController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import { CreateUserUseCase } from './CreateUserUseCase';
+
+export class CreateUserController {
+  constructor(private createUserUseCase: CreateUserUseCase) {}
+  async handle(request: Request, response: Response) {
+    const { name, email, password } = request.body;
+
+    try {
+      await this.createUserUseCase.execute({ name, email, password });
+
+      return response.status(201).send();
+    } catch (err) {
+      if (err instanceof Error)
+        return response
+          .status(400)
+          .json({ error: err.message || 'Unexpected error' });
+    }
+  }
+}
